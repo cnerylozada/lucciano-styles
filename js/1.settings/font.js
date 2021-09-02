5c86137efebacc2c9ca7f8aaac8c42392ba12334
cnerylozada <cnery@dynamicdevs.io>
2021-09-01 20:00:17 -0500

    feat: js styles were created

---
+++ js/1.settings/font.js
@@ -0,0 +1,26 @@
+export const fontSize = {
+    'headline-desktop': '28px',
+    'headline-mobile': '20px',
+  
+    'sectionHeadline-desktop': '23px',
+    'sectionHeadline-mobile': '18px',
+  
+    'subHeadline-desktop': '18px',
+    'subHeadline-mobile': '16px',
+  
+    'paragraph-desktop': '16px',
+    'paragraph-mobile': '14px',
+  
+    'small-desktop': '14px',
+    'small-mobile': '12px',
+  
+    'xsmall-desktop': '13px',
+    'xsmall-mobile': '10px'
+  }
+  
+  export const fontWeight = {
+    bold: '700',
+    regular: '400',
+    light: '300'
+  }
+  
---
+++ js/1.settings/layout.js
@@ -0,0 +1,12 @@
+const screenBreakpoints = {
+    xs: 0,
+    sm: '36rem',
+    md: '48rem',
+    lg: '62rem',
+    xl: '75rem'
+  }
+  
+  export const devices = {
+    'no-mobile': `(min-width: ${screenBreakpoints.sm})`
+  }
+  
