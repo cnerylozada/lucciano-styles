dc86ad0c9e7e340801032bc1484f25e7a5853cac
cnerylozada <cnery@dynamicdevs.io>
2021-09-21 07:08:09 -0500

    chore: prettier format

--- js/1.settings/color.js
+++ js/1.settings/color.js
@@ -1,9 +1,8 @@
 export const palette = {
-    black: '#223034',
-    blue: '#23a5ee',
-    red: '#df1b00',
-    green: '#00cc8f',
-    gray: '#ccc',
-    white: `#fff`
-  }
-  
+  black: '#223034',
+  blue: '#23a5ee',
+  red: '#df1b00',
+  green: '#00cc8f',
+  gray: '#ccc',
+  white: `#fff`
+}
--- js/1.settings/layout.js
+++ js/1.settings/layout.js
@@ -1,12 +1,11 @@
 const screenBreakpoints = {
-    xs: 0,
-    sm: '36rem',
-    md: '48rem',
-    lg: '62rem',
-    xl: '75rem'
-  }
-  
-  export const devices = {
-    'no-mobile': `(min-width: ${screenBreakpoints.sm})`
-  }
-  
+  xs: 0,
+  sm: '36rem',
+  md: '48rem',
+  lg: '62rem',
+  xl: '75rem'
+}
+
+export const devices = {
+  'no-mobile': `(min-width: ${screenBreakpoints.sm})`
+}
