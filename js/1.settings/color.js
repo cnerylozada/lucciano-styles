62eac3f5ceacbd85f3cbaae989a61231966c0462
cnerylozada <cnery@dynamicdevs.io>
2021-09-29 10:13:49 -0500

    feat: light and dark palette were added

--- js/1.settings/color.js
+++ js/1.settings/color.js
@@ -1,8 +1,17 @@
-export const palette = {
-  black: '#223034',
-  blue: '#23a5ee',
-  red: '#df1b00',
-  green: '#00cc8f',
-  gray: '#ccc',
-  white: `#fff`
+export const lightPalette = {
+  base: '#070707',
+  blue: '#42a5f5',
+  red: '#DF1B00',
+  green: '#0CC28C',
+  gray: '#CECECE',
+  bg: '#F1F1F1'
+}
+
+export const darkPalette = {
+  base: '#fff',
+  blue: '#00A3FF',
+  red: '#FF381D',
+  green: '#00EEA7',
+  gray: '#48585E',
+  bg: '#0C1214'
 }
