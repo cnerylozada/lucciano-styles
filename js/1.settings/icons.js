492bf38770cf36ec47cde112e76424401b05745c
cnerylozada <cnery@dynamicdevs.io>
2021-10-01 13:10:36 -0500

    feat: icon sizes were added

---
+++ js/1.settings/icons.js
@@ -0,0 +1,10 @@
+export const iconSize = {
+  'small-mobile': '15px',
+  'small-desktop': '24px',
+
+  'medium-mobile': '20px',
+  'medium-desktop': '30px',
+
+  'large-mobile': '25px',
+  'large-desktop': '35px'
+}
